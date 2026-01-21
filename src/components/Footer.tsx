8276afe3c38c9218404629229e73c52b6d75c50b
MagicMyData <magicmydata@MagicMyDatas-MacBook-Air.local>
2026-01-21 16:27:10 +0000

    Size down photo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
                 <Mail className="w-4 h-4" />
                 will@croydonpat.co.uk
               </a>
-              <img src={will} alt="Will @ Croydon PAT Photo" className="h-18" />
+              <img src={will} alt="Will @ Croydon PAT Photo" className="h-8" />
             </div>
           </div>
         </div>
